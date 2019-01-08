0e140e8ccc95278c1788fc3f0f4ab1d26f8d60ac
elin.nilsson <elinhejnilsson@gmail.com>
2019-01-08 14:54:58 +0100

    Minor fix.

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ new Vue({
             this.playerHealth = 100;
             this.monsterHealth = 100;
             this.message = null;
-            this.actions.length = [];
+            this.actions = [];
             this.gameIsPlaying = true;
         },
         monsterAttack(){
